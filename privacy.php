619e2687b620fbfdf388c3ad0731e09fc8911c5b
Yassine6262 <yassineaz2018@outlook.com>
2023-03-10 13:25:39 +0100

    d

---
+++ privacy.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <title>CPC Kicks</title>
+</head>
+<body>
+
+<?php
+   include 'navbar.php'; ?>
+   <h2 id="prv">Privacy Policy</h2>
+<p id="p2"></p> 
+Privacyverklaring <br>
+<br>
+Wij respecteren uw privacy en hechten waarde aan de bescherming van uw persoonlijke gegevens. In deze privacyverklaring leggen we uit hoe wij uw persoonlijke gegevens verzamelen, gebruiken en beschermen. <br>
+<br>
+Verzameling van persoonlijke gegevens
+Wij verzamelen alleen persoonlijke gegevens die u vrijwillig aan ons verstrekt, bijvoorbeeld wanneer u zich registreert voor een account, een product koopt of contact met ons opneemt via e-mail of telefoon. Deze gegevens kunnen uw naam, e-mailadres, adres, telefoonnummer en betaalgegevens omvatten. <br>
+<br>
+Gebruik van persoonlijke gegevens
+Wij gebruiken uw persoonlijke gegevens alleen voor de doeleinden waarvoor ze zijn verzameld, zoals het verwerken van uw bestellingen, het beheren van uw account, het beantwoorden van uw vragen of het versturen van marketingcommunicatie waarvoor u zich heeft aangemeld. Wij zullen uw persoonlijke gegevens niet delen met derden, behalve zoals nodig is voor het uitvoeren van onze diensten, zoals het verzenden van producten via een bezorgdienst. <br>
+<br>
+Beveiliging van persoonlijke gegevens
+Wij nemen de beveiliging van uw persoonlijke gegevens serieus en hebben passende technische en organisatorische maatregelen genomen om uw persoonlijke gegevens te beschermen tegen ongeautoriseerde toegang, wijziging, openbaarmaking of vernietiging. <br>
+<br>
+Bewaartermijn persoonlijke gegevens
+Wij bewaren uw persoonlijke gegevens alleen zo lang als nodig is voor de doeleinden waarvoor ze zijn verzameld of zoals vereist door de wet. <br>
+<br>
+Toegang tot en controle over uw persoonlijke gegevens
+U heeft het recht om uw persoonlijke gegevens in te zien, te corrigeren, te laten verwijderen of de verwerking ervan te beperken. U kunt ook bezwaar maken tegen de verwerking van uw persoonlijke gegevens voor direct marketingdoeleinden. Neem hiervoor contact met ons op via de contactgegevens onderaan deze pagina. <br>
+<br>
+Cookies
+Wij maken gebruik van cookies om uw gebruik van onze website te verbeteren en om statistieken bij te houden over het gebruik van onze website. Een cookie is een klein tekstbestand dat op uw computer of mobiele apparaat wordt geplaatst wanneer u onze website bezoekt. U kunt uw browser zo instellen dat cookies worden geweigerd of dat u wordt gewaarschuwd wanneer er cookies worden geplaatst. <br>
+<br>
+Wijzigingen in de privacyverklaring
+Wij behouden ons het recht voor om deze privacyverklaring op elk moment te wijzigen. Controleer regelmatig deze pagina om op de hoogte te blijven van eventuele wijzigingen. <br>
+<br>
+Contactgegevens
+Als u vragen of opmerkingen heeft over onze privacyverklaring of de manier waarop wij uw persoonlijke gegevens verzamelen, gebruiken of beheren, neem dan contact met ons op via [contactgegevens].
