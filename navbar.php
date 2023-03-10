ea53c198f4777e83a7a29d35460089347c673736
Ouail06 <9019064@student.zadkine.nl>
2023-03-10 09:52:31 +0100

    uvhjnhhj

--- navbar.php
+++ navbar.php
@@ -9,18 +9,20 @@
             <nav>
                 <ul>
                     <li><a href="index.php">Home</a></li>
-                    <li><a href="">Featured</a></li>
-                    <li><a href="">Product</a></li>
                     <div class="dropdown">
-    <button onclick="myFunction()" class="dropbtn">Categorieen</button>
-    <div id="myDropdown" class="dropdown-content">
-        <a href="#">Heren</a> <br>
-        <a href="#">Dames</a> <br>
-        <a href="#">Jongens</a> <br>
-        <a href="#">Dames</a> <br>
-        <a href="#">Baby's</a> <br>
+                    <li><a href="">categorieen</a></li>
+        <div id="myDropdown" class="dropdown-content">
+        
+            <a href="#">Heren</a> <br>
+            <a href="#">Dames</a> <br>
+            <a href="#">Jongens</a> <br>
+            <a href="#">Meisjes</a> <br>
+            <a href="#">Baby's</a> <br>
   </div>
 </div>
+                    <li><a href="">Featured</a></li>
+                    <li><a href="">Product</a></li>
+                    
       </li>
                     <li><a href="overons.php">Over Ons</a></li>
                     <li><a href="account.php">Account</a></li>
