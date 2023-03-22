572a870a1800cbc5b1b727da8d452e4344edc538
Yassine6262 <yassineaz2018@outlook.com>
2023-03-22 11:30:05 +0100

    project3

--- navbar.php
+++ navbar.php
@@ -1,3 +1,4 @@
+
 <html>
     <head>
         <link rel="stylesheet" href="dropdown.css">
@@ -5,7 +6,7 @@
     <body>
 <div class="container">
         <div class="navbar">
-            <img src="images/logo.png" class="logo">
+            <img src="./images/Logo.png" class="logo">
             <nav>
                 <ul>
                     <li><a href="index.php">Home</a></li>
@@ -32,6 +33,3 @@
         </div>
 </body>
         </html>
-
-<?php
-?>
