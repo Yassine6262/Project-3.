5ac7ba0b2cb5873746430c3bb51ae243ca046856
Yassine6262 <yassineaz2018@outlook.com>
2023-04-12 12:09:28 +0200

    ;

--- navbar.php
+++ navbar.php
@@ -13,6 +13,7 @@
                     <li><a href="product.php">collection</a></li>
                     <li><a href="overons.php">Over Ons</a></li>
                     <li><a href="help.php">Help</a></li>
+                    <li><a href="account.php">Account</a></li>
                     
                 </ul>
             </nav>
